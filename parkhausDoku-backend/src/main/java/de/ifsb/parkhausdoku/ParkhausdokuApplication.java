5f3797e7aa41d21ead7bbac8ea3d5ab8dde56fe9
Christian Smit <chrsmit@web.de>
2020-07-12 10:51:30 +0200

    Initial commit Project setup

---
+++ parkhausDoku-backend/src/main/java/de/ifsb/parkhausdoku/ParkhausdokuApplication.java
@@ -0,0 +1,13 @@
+package de.ifsb.parkhausdoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkhausdokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkhausdokuApplication.class, args);
+    }
+
+}
---
+++ parkhausDoku-backend/src/test/java/de/ifsb/parkhausdoku/ParkhausdokuApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ifsb.parkhausdoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkhausdokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
