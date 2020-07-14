e784afe956f81e7793a36d758b28a716ab423ade
Christian Smit <chrsmit@web.de>
2020-07-14 13:20:54 +0200

    Add spring mongodb id to admission model

--- parkhausDoku-backend/src/main/java/de/ifsb/parkhausdoku/model/Admission.java
+++ parkhausDoku-backend/src/main/java/de/ifsb/parkhausdoku/model/Admission.java
@@ -2,11 +2,13 @@ package de.ifsb.parkhausdoku.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
 public class Admission {
 
+    @Id
     private String id;
     private String information;
 
