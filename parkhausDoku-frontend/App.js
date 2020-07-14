4a493ad786589a06accbc75ebb0bc6571bc21953
Christian Smit <chrsmit@web.de>
2020-07-14 09:42:02 +0200

    Protect Master Branch

--- parkhausDoku-frontend/App.js
+++ parkhausDoku-frontend/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test for protexted Master branch</Text>
+      <Text>Test for protexted Master branch again</Text>
       <StatusBar style="auto" />
     </View>
   );
