7a8cb3507e8f0bfb7e0f1490aea5f03403595889
Christian Smit <chrsmit@web.de>
2020-07-14 13:19:55 +0200

    Add endpoint getAdmissionById

--- parkhausDoku-backend/src/main/java/de/ifsb/parkhausdoku/service/AdmissionService.java
+++ parkhausDoku-backend/src/main/java/de/ifsb/parkhausdoku/service/AdmissionService.java
@@ -1,18 +1,18 @@
 package de.ifsb.parkhausdoku.service;
 
+import de.ifsb.parkhausdoku.db.AdmissionMongoDb;
 import de.ifsb.parkhausdoku.model.Admission;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class AdmissionService {
+    private final AdmissionMongoDb admissionDb;
 
-
-    public static Admission getAdmissionById(String id) {
-        return new Admission(
-                id,
-                "some Info"
-        );
+    public Optional<Admission> getAdmissionById(String id) {
+        return admissionDb.findById(id);
     }
 }
