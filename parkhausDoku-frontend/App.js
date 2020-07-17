93f8f39d70061d7e5682079a30d1e9e610efc77e
Christian Smit <chrsmit@web.de>
2020-07-17 17:32:57 +0200

    Add component Plan2D displays svg as fullscreen

--- parkhausDoku-frontend/App.js
+++ parkhausDoku-frontend/App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaProvider} from "react-native-safe-area-context";
+import Plan2D from "./components/2DPlan/Plan2D";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Test for protexted Master branch again</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaProvider forceInset={{ top: 'always' }}>
+        <View style={styles.container}>
+            <Plan2D/>
+        </View>
+      </SafeAreaProvider>
   );
 }
 
@@ -16,6 +19,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
---
+++ parkhausDoku-frontend/components/2DPlan/Plan2D.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SvgUri} from "react-native-svg";
+
+export default function Plan2D(){
+    return (
+        <SvgUri
+            width="100%"
+            height="100%"
+            uri="http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg"
+        />
+    )
+}
