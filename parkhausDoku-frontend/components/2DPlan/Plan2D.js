552f57c016a5bda1c70f9524df955e5e3daf37b4
Christian Smit <chrsmit@web.de>
2020-07-19 14:03:31 +0200

    Add Plan2D display 2d svd plan as component

--- parkhausDoku-frontend/components/2DPlan/Plan2D.js
+++ parkhausDoku-frontend/components/2DPlan/Plan2D.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {SvgUri} from "react-native-svg";
 
-export default function Plan2D(){
+export default function Plan2D({url}){
     return (
         <SvgUri
             width="50%"
             height="50%"
-            uri="http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg"
+            uri={url}
         />
     )
 }
