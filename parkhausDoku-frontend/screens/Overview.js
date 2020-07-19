06f289c62ad0ac2a8b0ebf3638598b049038fd3e
Christian Smit <chrsmit@web.de>
2020-07-19 14:06:46 +0200

    Add Screen overview

--- parkhausDoku-frontend/screens/Overview.js
+++ parkhausDoku-frontend/screens/Overview.js
@@ -5,7 +5,7 @@ import Plan2D from "../components/2DPlan/Plan2D";
 export default function Overview() {
     return (
             <View>
-                <Plan2D></Plan2D>
+                <Plan2D/>
                 <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, minima?</Text>
             </View>
 
