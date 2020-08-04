43abe524491f236a12a0331c7d4c43c38fe894ea
Christian Smit <chrsmit@web.de>
2020-08-04 19:12:49 +0200

    removed level-saga not as previously mentioned Building saga <- this was a naming error

--- parkhausdoku-frontend/src/redux/levels/level-reducer.js
+++ parkhausdoku-frontend/src/redux/levels/level-reducer.js
@@ -4,14 +4,10 @@ import {SET_LEVELS, SET_LEVELS_FAILED, SET_LEVELS_SUCCESS} from "../utils/level-
 const levelReducer = (state = {status: null,levels:[]}, action) => {
     switch (action.type) {
         case SET_LEVELS:
-            return {status: 'PENDING'}
-        case SET_LEVELS_SUCCESS:
             return {
                 status:'SUCCESS',
                 levels: action.payload,
             }
-        case SET_LEVELS_FAILED:
-            return {status:'FAILED'}
         default:
             return state
     }
--- parkhausdoku-frontend/src/redux/sagas.js
+++ parkhausdoku-frontend/src/redux/sagas.js
@@ -1,14 +1,12 @@
 import {all} from "@redux-saga/core/effects";
 import {authSaga} from "./auth/auth-saga";
 import {buildingSaga} from "./buildings/building-saga";
-import {levelSaga} from "./levels/level-saga";
 
 
 export default function* rootSaga() {
     yield all(
         [...authSaga,
             ...buildingSaga,
-            ...levelSaga,
         ]
     )
 }
